aa2a0950a1b99fe070ce2751c00fa671828e9863
Andrew Tian <andrew@tian.io>
2016-01-23 14:42:51 -0800

    working req list

--- server.go
+++ server.go
@@ -6,20 +6,31 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"sync"
 	"text/template"
+	"time"
 )
 
 const (
-	_targetURL  = "http://www.cloudflare.com:443"
+	_targetURL  = "https://www.cloudflare.com:443"
 	templateDir = "templates/"
 )
 
+type RequestEvent struct {
+	Req        *http.Request
+	RecordedAt time.Time
+}
+
 type LookingGlass struct {
-	proxy *httputil.ReverseProxy
+	proxy      *httputil.ReverseProxy
+	requestLog []*RequestEvent
+	mutex      *sync.RWMutex
 }
 
 func NewLookingGlass(target *url.URL) *LookingGlass {
 	return &LookingGlass{
+		requestLog: []*RequestEvent{},
+		mutex:      &sync.RWMutex{},
 		proxy: &httputil.ReverseProxy{
 			Director: func(r *http.Request) {
 				// r.URL.Host = "cloudflare.com"
@@ -37,6 +48,22 @@ func NewLookingGlass(target *url.URL) *LookingGlass {
 
 func (lg *LookingGlass) process(r *http.Request) {
 	log.Print(fmt.Sprintf("lookingglass: logged %s\n", r.URL))
+	event := &RequestEvent{
+		Req:        r,
+		RecordedAt: time.Now(),
+	}
+
+	lg.mutex.Lock()
+	lg.requestLog = append(lg.requestLog, event)
+	lg.mutex.Unlock()
+}
+
+func (lg *LookingGlass) Requests() []*RequestEvent {
+	lg.mutex.RLock()
+	reqs := lg.requestLog
+	lg.mutex.RUnlock()
+
+	return reqs
 }
 
 func (lg *LookingGlass) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -44,12 +71,14 @@ func (lg *LookingGlass) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	lg.proxy.ServeHTTP(w, r)
 }
 
+var lg *LookingGlass
+
 func main() {
 	target, err := url.Parse(_targetURL)
 	if err != nil {
 		panic(err)
 	}
-	lg := NewLookingGlass(target)
+	lg = NewLookingGlass(target)
 
 	http.Handle("/", lg)
 	http.HandleFunc("/stats", StatsHandler)
@@ -57,8 +86,10 @@ func main() {
 }
 
 func StatsHandler(w http.ResponseWriter, r *http.Request) {
+	reqs := lg.Requests()
 	p := map[string]interface{}{
-		"Data": "asdf",
+		"Requests": reqs,
+		"Max":      len(reqs),
 	}
 
 	tmpl, _ := template.ParseFiles(templateDir + "index.html")
